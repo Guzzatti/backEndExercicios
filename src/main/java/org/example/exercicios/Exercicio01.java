c500f0ea437de20f66b88c0cb2426b7d36bea9fe
Gabriel Guzzatti <136494543+Guzzatti@users.noreply.github.com>
2024-08-13 21:42:27 -0300

    feat: adicionado exercicio 2

--- src/main/java/org/example/exercicios/Exercicio01.java
+++ src/main/java/org/example/exercicios/Exercicio01.java
@@ -1,7 +1,41 @@
 package org.example.exercicios;
 
+import java.util.Scanner;
+
 public class Exercicio01 {
     public static void main() {
-        System.out.println("Estou executando o exercício 2");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Que ambientação você prefere?");
+        System.out.println("1. Sci-fi");
+        System.out.println("2. Medieval");
+        int ambientacao = scanner.nextInt();
+
+        if (ambientacao != 1 && ambientacao != 2) {
+            System.out.println("Escolha inválida. O programa será encerrado.");
+            return;
+        }
+
+        System.out.println("Que gênero você prefere?");
+        System.out.println("1. Comédia");
+        System.out.println("2. Drama");
+        int genero = scanner.nextInt();
+
+        if (genero != 1 && genero != 2) {
+            System.out.println("Escolha inválida. O programa será encerrado.");
+            return;
+        }
+
+        if (ambientacao == 1 && genero == 1) {
+            System.out.println("Recomendação: Homens de Preto");
+        } else if (ambientacao == 1 && genero == 2) {
+            System.out.println("Recomendação: Arrival");
+        } else if (ambientacao == 2 && genero == 1) {
+            System.out.println("Recomendação: Shrek");
+        } else if (ambientacao == 2 && genero == 2) {
+            System.out.println("Recomendação: Gladiador");
+        }
+
+        scanner.close();
     }
 }
