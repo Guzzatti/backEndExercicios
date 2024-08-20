ecd1b5fd261007c5815b2c3066ed765b4077c7dc
Gabriel Guzzatti <136494543+Guzzatti@users.noreply.github.com>
2024-08-20 20:44:15 -0300

    update01

---
+++ exercicio00/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import org.example.exercicios.Exercicio00;
+
+public class Main {
+    public static void main(String[] args) {
+        Exercicio00.main();
+    }
+}
---
+++ exercicio01/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import org.example.exercicios.Exercicio01;
+
+public class Main {
+    public static void main(String[] args) {
+        Exercicio01.main();
+    }
+}
