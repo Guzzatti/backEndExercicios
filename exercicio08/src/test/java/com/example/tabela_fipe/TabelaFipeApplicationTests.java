6bd2cbd1c23bc635c10ce049f562662e184eb0fa
Gabriel Guzzatti <136494543+Guzzatti@users.noreply.github.com>
2024-09-17 21:53:59 -0300

    update

---
+++ exercicio08/src/test/java/com/example/tabela_fipe/TabelaFipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tabela_fipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabelaFipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
