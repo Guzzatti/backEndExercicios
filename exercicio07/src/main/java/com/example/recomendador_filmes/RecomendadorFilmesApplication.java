cecbcba9980d9bff42e347a49211d24fd122d26b
Gabriel Guzzatti <136494543+Guzzatti@users.noreply.github.com>
2024-09-17 21:12:12 -0300

    update

---
+++ exercicio07/src/main/java/com/example/recomendador_filmes/RecomendadorFilmesApplication.java
@@ -0,0 +1,13 @@
+package com.example.recomendador_filmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecomendadorFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecomendadorFilmesApplication.class, args);
+	}
+
+}
