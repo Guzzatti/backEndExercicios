2ec208d59a8523470f0d4b02688da89978d05b24
Gabriel Guzzatti <136494543+Guzzatti@users.noreply.github.com>
2024-10-14 19:29:33 -0300

    ok11

---
+++ exercicio11/src/main/java/com/example/geradorjogador/GeradorjogadorApplication.java
@@ -0,0 +1,13 @@
+package com.example.geradorjogador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorjogadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorjogadorApplication.class, args);
+	}
+
+}
---
+++ exercicio11/src/test/java/com/example/geradorjogador/GeradorjogadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.geradorjogador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeradorjogadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
