c23fb1fb839ff255b6f94ff4538c889eec3d5131
Gabriel Guzzatti <136494543+Guzzatti@users.noreply.github.com>
2024-10-14 19:00:40 -0300

    update

---
+++ exercicio11/src/main/java/com/example/example11/Example11Application.java
@@ -0,0 +1,13 @@
+package com.example.example11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Example11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Example11Application.class, args);
+	}
+
+}
---
+++ exercicio11/src/test/java/com/example/example11/Example11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.example11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Example11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
